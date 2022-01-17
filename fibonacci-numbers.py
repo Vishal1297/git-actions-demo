55f12c90ef95bfbd68b95346465beea594ce9af1
Vishal <vishalyadav1746@gmail.com>
2022-01-17 09:31:38 +0530

    feat: get fibonacci number.

---
+++ fibonacci-numbers.py
@@ -0,0 +1,12 @@
+# Get fibonaaci numbers
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(0, 10):
+print(fibonacci(i))
